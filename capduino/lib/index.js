b158d8d8531647236bbb8e8dc19db177b896eaae
Hunter Clarke <hunter@hunterc.com>
2013-04-19 21:38:20 -0400

    Cleaned up a whole bunch of code and simplified the arduino code.

--- capduino/lib/index.js
+++ capduino/lib/index.js
@@ -10,15 +10,7 @@ var capduino = function(options) {
 	options.serialport = options.serialport || "/dev/tty-usbserial1";
 	options.baudrate = options.baudrate || 115200;
 
-	self.INIT       = "00";
-	self.ERROR      = "01";
-	self.PING       = "02";
-	self.PONG       = "03";
-	self.GET        = "04";
-	self.SET        = "05";
-	self.UNWATCH    = "07";
 	self.WATCHCAP		= "08";
-	self.OK         = "23";
 
 	self.sp = new serial.SerialPort(options.serialport, {
 		parser : serial.parsers.readline('\n'),
@@ -48,108 +40,60 @@ var capduino = function(options) {
 		while (pos < data.length) {
 			// comment
 			if (data[pos] == '#') {
-			comment = data.substring(pos + 1);
+				comment = data.substring(pos + 1);
+				break;
+			}
+
+			// delimiter
+			else if (data[pos] == ':') {
+				var scount = 0;
+					pos++;
+					var ssize = "";
+
+				while (data[pos] != ':') {
+					ssize += data[pos];
+					scount++;
+					pos++;
+
+					if (scount == 5) {
+						self.send(self.ERROR, [ "Argument Too Long", data ]);
+						return;
+					}
+				}
+
+				if (scount) {
+					if (parseInt(ssize, 10) >= self.MAX_BUFFER) {
+						self.send(self.ERROR, [ "Argument Too Long", data ]);
+						return;
+					}
+
+					pos++;
+					args[count] = "";
+					for (var i = pos; i < pos + parseInt(ssize, 10); i++) {
+						args[count] += data[i];
+					}
+
+					count++;
+					pos += parseInt(ssize, 10) - 1;
+				}
+			} else {
+				//self.send(self.ERROR, [ "Invalid Argument", count ]);
+				return;
+			}
+
+			pos++;
+		}
+
+		switch (command) {
+
+		case self.WATCHCAP:
+			self.emit('watchcap', args, comment);
 			break;
+
+		default:
+			console.log("default: " + command + " => " + data);
+			//self.send(self.ERROR, [ "Unknown Command", command ]);
 		}
-
-		// delimiter
-		else if (data[pos] == ':') {
-			var scount = 0;
-				pos++;
-				var ssize = "";
-
-			while (data[pos] != ':') {
-				ssize += data[pos];
-				scount++;
-				pos++;
-
-				if (scount == 5) {
-					self.send(self.ERROR, [ "Argument Too Long", data ]);
-					return;
-				}
-			}
-
-			if (scount) {
-				if (parseInt(ssize, 10) >= self.MAX_BUFFER) {
-					self.send(self.ERROR, [ "Argument Too Long", data ]);
-					return;
-				}
-
-				pos++;
-				args[count] = "";
-				for (var i = pos; i < pos + parseInt(ssize, 10); i++) {
-					args[count] += data[i];
-				}
-
-				count++;
-				pos += parseInt(ssize, 10) - 1;
-			}
-		} else {
-			//self.send(self.ERROR, [ "Invalid Argument", count ]);
-			return;
-		}
-
-		pos++;
-	}
-
-	switch (command) {
-		case self.PONG:
-		self.emit('pong');
-		break;
-
-	case self.INIT:
-		self.emit('init', args, comment);
-		break;
-
-	case self.PONG:
-		self.emit('pong', args, comment);
-		break;
-
-	case self.GET:
-		self.emit('get', args, comment);
-		break;
-
-	case self.OK:
-		self.emit('ok', args, comment);
-		break;
-
-	case self.WATCHCAP:
-		self.emit('watchcap', args, comment);
-		break;
-
-	case self.ERROR:
-		self.emit('exception', args, comment);
-		break;
-
-	default:
-		console.log("default: " + command + " => " + data);
-		//self.send(self.ERROR, [ "Unknown Command", command ]);
-		}
-	};
-
-	self.send = function (command, data, comment) {
-		var packet = command;
-
-		for (var i = 0; data && i < data.length; i++) {
-			packet += ':' + data[i].toString().length + ':' + data[i];
-		}
-
-		if (comment) {
-			packet += '#' + comment;
-		}
-
-		packet += "\n";
-		self.sp.write(packet);
-	};
-
-
-	self.watchcap = function(sendPin, receivePin) {
-		console.log('Sending watchap');
-		self.send(self.WATCHCAP, [sendPin, receivePin]);
-	};
-
-	self.ping = function() {
-		self.send(self.PING, []);
 	};
 };
 
--- capduino/src/iva/iva.ino
+++ capduino/src/iva/iva.ino
@@ -1,19 +1,7 @@
 #define NUMSENSORS 3
 #define BAUDRATE   115200
-
-#define INIT       "00"
-#define ERROR      "01"
-#define PING       "02"
-#define PONG       "03"
-#define GET        "04"
-#define SET        "05"
-#define WATCH      "06"
-#define UNWATCH    "07"
 #define WATCHCAP   "08"
-#define OK         "23"
-
 #define MAX_BUFFER 1024
-#define MAX_ARGS   10
 
 #include <stdarg.h>
 #include <cstring>
@@ -33,16 +21,23 @@ int sensorsSet = 0;
 capsensor sensors[NUMSENSORS];
 
 void setup () {
-  //Serial.begin(BAUDRATE);
-  //Serial.println();
   bt.begin(BAUDRATE);
   bt.println();
-  write(INIT, NULL, 0, "");
+  char *args[2];
+  args[0] = "31";
+  args[1] = "32";
+  cmd_watch_cap(args, 2);
+
+  args[0] = "33";
+  args[1] = "34";
+  cmd_watch_cap(args, 2);
+
+  args[0] = "35";
+  args[1] = "36";
+  cmd_watch_cap(args, 2);
 }
 
 void loop () {
-  // serial read and command processing loop
-  read();
 
   int i;
 
@@ -73,83 +68,24 @@ void loop () {
       tag
     };
 
-    write(WATCHCAP, data, 2, NULL);
+    write(WATCHCAP, data, 2);
   }
-}
-
-// read any packets waiting, yielding every 10 characters
-void read () {
-  static char buffer[MAX_BUFFER];
-  static int index = 0;
-  static int count = 0;
-
-  while (bt.available() > 0 && count < 10) {
-    if (index >= MAX_BUFFER) {
-      char *data[1];
-
-      data[0] = "Maximum Buffer Size Exceeded";
-      write(ERROR, data, 1, NULL);
-
-      index = 0;
-      count = 0;
-
-      return;
-    }
-
-    buffer[index] = bt.read();
-
-    if (buffer[index] == '\n') {
-      buffer[index] = '\0';
-      execute(buffer);
-
-      index = 0;
-
-      return;
-    }
-
-    index++;
-    count++;
-  }
-
-  count = 0;
 }
 
 // build the packet and send via serial
-void write (char *command, char *args[], int count, char *comment) {
+void write (char *command, char *args[], int count) {
   int i;
-
-  /*Serial.write(command);
-
-  if (args && count) {
-    for (i = 0; i < count; i++) {
-      Serial.write(":");
-      Serial.print(strlen(args[i]));
-      Serial.write(":");
-      Serial.write(args[i]);
-    }
-  }
-
-  if (comment) {
-    Serial.write("#");
-    Serial.write(comment);
-  }
-
-  Serial.write("\n");*/
 
   bt.write(command);
 
   if (args && count) {
+    bt.write("");
     for (i = 0; i < count; i++) {
       bt.write(":");
       bt.print(strlen(args[i]));
       bt.write(":");
       bt.write(args[i]);
     }
-  }
-
-  if (comment) {
-    bt.write("#");
-    bt.write(comment);
   }
 
   bt.write("\n");
@@ -163,16 +99,6 @@ void write (char *command, char *args[], int count, char *comment) {
 void cmd_watch_cap (char *args[], int count) {
   capsensor sensor;
 
-  /*Serial.println("Args:");
-  Serial.println(args[0]);
-  Serial.println(args[1]);
-  Serial.println("Done");*/
-
-  /*bt.println("Args:");
-  bt.println(args[0]);
-  bt.println(args[1]);
-  bt.println("Done");*/
-
   sensor.isActivated = 1;
   sensor.sendPin = atoi(args[0]); //31
   sensor.receivePin = atoi(args[1]); //33
@@ -182,134 +108,4 @@ void cmd_watch_cap (char *args[], int count) {
 
   sensors[sensorsSet] = sensor;
   sensorsSet = sensorsSet + 1;
-
-  write(OK, NULL, 0, NULL);
-}
-
-void cmd_ping (char *args[], int count) {
-  write(PONG, NULL, 0, NULL);
-}
-
-// parse the packet and execute the commands
-void execute (char *buffer) {
-  char command[3];
-  char *args[MAX_ARGS];
-  int count = 0;
-
-  if (strlen(buffer) < 2) {
-    char *data[2];
-
-    data[0] = "Malformed Command";
-    data[1] = buffer;
-    write(ERROR, data, 2, NULL);
-  } else {
-    command[0] = buffer[0];
-    command[1] = buffer[1];
-    command[2] = '\0';
-
-    int pos = 2;
-    int length = strlen(buffer);
-
-    while (count < MAX_ARGS && pos < length) {
-      // comment
-      if (buffer[pos] == '#') {
-        break;
-      }
-
-      // delimiter
-      else if (buffer[pos] == ':') {
-        char ssize[5];
-        int scount = 0;
-
-        pos++;
-
-        // read the length of the argument
-        while (buffer[pos] != ':') {
-          ssize[scount] = buffer[pos];
-          scount++;
-          pos++;
-
-          if (scount == 5) {
-            char *data[2];
-
-            data[0] = "Argument Too Long";
-            data[1] = buffer;
-
-            write(ERROR, data, 2, NULL);
-            cleanup(args, count);
-
-            return;
-          }
-        }
-
-        if (scount) {
-          if (atoi(ssize) >= MAX_BUFFER) {
-            char *data[2];
-
-            data[0] = "Argument Too Long";
-            data[1] = buffer;
-
-            write(ERROR, data, 2, NULL);
-            cleanup(args, count);
-
-            return;
-          }
-
-          pos++;
-          int i;
-
-          ssize[scount] = '\0';
-
-          // allocate and copy the argument now that we have the size
-          args[count] = (char *) malloc(sizeof(char) * (atoi(ssize) + 1));
-          for (i = pos; i < pos + atoi(ssize); i++) {
-            args[count][i - pos] = buffer[i];
-          }
-
-          args[count][i - pos] = '\0';
-          count++;
-          pos += atoi(ssize) - 1;
-        }
-      } else {
-        char *data[2];
-
-        data[0] = "Invalid Argument";
-        //data[1] = p("%d", count);
-
-        write(ERROR, data, 2, NULL);
-
-        cleanup(args, count);
-        free(data[1]);
-
-        return;
-      }
-
-      pos++;
-    }
-
-    if (strcmp(command, WATCHCAP) == 0) {
-      cmd_watch_cap(args, count);
-    } else if (strcmp(command, PING) == 0) {
-      cmd_ping(args, count);
-    } else {
-      char *data[2];
-
-      data[0] = "Unknown Command";
-      data[1] = command;
-
-      write(ERROR, data, 2, NULL);
-    }
-
-    cleanup(args, count);
-  }
-}
-
-// free up any memory allocated from the packet
-void cleanup (char **args, int count) {
-  int i;
-  for (i = 0; i < count; i++) {
-    if (args[i] != NULL) {
-      free(args[i]);
-    }
-  }
 }
